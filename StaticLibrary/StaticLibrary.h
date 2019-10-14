5c806687238b0afb345b22297eb0806842d83345
杨俊艺 <1209032479@qq.com>
2019-10-14 18:53:29 +0800

    Initial Commit

---
+++ StaticLibrary/StaticLibrary.h
@@ -0,0 +1,13 @@
+//
+//  StaticLibrary.h
+//  StaticLibrary
+//
+//  Created by 杨俊艺 on 2019/10/14.
+//  Copyright © 2019 杨俊艺. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StaticLibrary : NSObject
+
+@end
