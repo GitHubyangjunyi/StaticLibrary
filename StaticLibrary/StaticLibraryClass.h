48551b11c0da972e8535a18c849af9dc9eb583df
杨俊艺 <1209032479@qq.com>
2019-10-14 19:39:44 +0800

    sync

---
+++ StaticLibrary/StaticLibraryClass.h
@@ -0,0 +1,19 @@
+//
+//  StaticLibraryClass.h
+//  StaticLibrary
+//
+//  Created by 杨俊艺 on 2019/10/14.
+//  Copyright © 2019 杨俊艺. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StaticLibraryClass : NSObject
+
+-(void)demoOutput;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ StaticLibrary/StaticLibraryClass.m
@@ -0,0 +1,18 @@
+//
+//  StaticLibraryClass.m
+//  StaticLibrary
+//
+//  Created by 杨俊艺 on 2019/10/14.
+//  Copyright © 2019 杨俊艺. All rights reserved.
+//
+
+#import "StaticLibraryClass.h"
+
+@implementation StaticLibraryClass
+
+-(void)demoOutput
+{
+    NSLog(@"StaticLibraryClass output");
+}
+
+@end
